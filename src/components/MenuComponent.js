d682d17523d440449064fd1e68bba78fc1162b08
jaffasunny <jaffer.sunny125@gmail.com>
2021-09-10 15:43:04 +0500

    Assignment 1 completed

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
+import DishDetail from "./DishdetailComponent ";
+
 import {
 	Card,
 	CardImg,
@@ -14,37 +15,21 @@ class Menu extends Component {
 		super(props);
 
 		this.state = {
-            selectedDish : null
-        };
+			selectedDish: null,
+		};
+		console.log('menu component constructed')
 	}
 
-    onDishSelect(dish){
-        this.setState({ selectedDish : dish });
-    }
-
-    renderDish(dish){
-        if(dish != null){
-            return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }else{
-            return(
-                <div></div>
-            );
-        }
-    }
+	onDishSelect(dish) {
+		this.setState({ selectedDish: dish });
+	}
 
 	render() {
+		console.log('renders menu component')
 		const menu = this.props.dishes.map((dish) => {
 			return (
 				<div key={dish.id} className="col-12 col-md-5 m-1">
-					<Card onClick={()=>this.onDishSelect(dish)}>
+					<Card onClick={() => this.onDishSelect(dish)}>
 						<CardImg width="100%" src={dish.image} alt={dish.name} />
 						<CardImgOverlay>
 							<CardTitle>{dish.name}</CardTitle>
@@ -56,11 +41,12 @@ class Menu extends Component {
 		return (
 			<div className="container">
 				<div className="row">{menu}</div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
-                </div>
+				<DishDetail dish={this.state.selectedDish} />
 			</div>
 		);
+	}
+	componentDidMount(){
+		console.log('Menu Component componentDidMount is invoked')
 	}
 }
 
